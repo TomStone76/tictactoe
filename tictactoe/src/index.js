12274b2f462f6ee704be1611217f9dfc6ded9354
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-27 23:37:05 -0500

    commit

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -2,16 +2,23 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        }
+    }
+
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => { alert('click'); }}>
                 {this.props.value}
             </button>
         )
     }
 }
 
-class Board extends React.component {
+class Board extends React.Component {
     renderSquare(i) {
         return <Square value={i}/>;
     }

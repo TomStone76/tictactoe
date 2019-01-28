f8907eee141455c8f06ff759107a04f8d9e3976d
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-27 23:28:31 -0500

    commit

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -43,9 +43,22 @@ class Board extends React.component {
 }
 
 class Game extends React.Component {
-
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                 <Board />   
+                </div>
+                <div className="game-info">
+                    <div>{}</div>
+                    <ol>{}</ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-
+    <Game />,
+    document.getElementById('root')
 );

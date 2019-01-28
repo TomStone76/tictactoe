63da009f9229ca1bc56607f1cb13219a35e75b73
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-27 23:31:07 -0500

    commit

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -5,7 +5,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square">
-                {}
+                {this.props.value}
             </button>
         )
     }
@@ -13,7 +13,7 @@ class Square extends React.Component {
 
 class Board extends React.component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i}/>;
     }
 
     render() {
